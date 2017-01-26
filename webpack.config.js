9e318112f9bf6e523de0cf59bcbd5573dc57d2ca
Tamas Oberritter <oberritter.tamas@gmail.com>
2017-01-27 00:39:27 +0100

    vue-test

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/js/app.js',
+    vendor: ['vue']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'js'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: [['es2015', { 'modules': false }]] }
+        }]
+      },
+      {
+        test: /\.vue$/,
+        use: [{
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+              // the "scss" and "sass" values for the lang attribute to the right configs here.
+              // other preprocessors should work out of the box, no loader config like this nessessary.
+              'scss': 'vue-style-loader!css-loader!sass-loader',
+              'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            }
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor' 
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+      exclude: 'vendor'
+    })
+  ]
+}
